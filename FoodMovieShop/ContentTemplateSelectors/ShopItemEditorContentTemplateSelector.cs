56f957cfd23d7a858d3aa8826654a944dfbf021f
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2021-02-24 17:58:15 +0100

    Implemented Editors

---
+++ FoodMovieShop/ContentTemplateSelectors/ShopItemEditorContentTemplateSelector.cs
@@ -0,0 +1,50 @@
+﻿using FoodMovieShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FoodMovieShop.ContentTemplateSelectors
+{
+    public class ShopItemEditorContentTemplateSelector : DataTemplateSelector
+    {
+
+        public DataTemplate FoodEditorTemplate 
+        {
+            get;
+            set;
+        }
+
+        public DataTemplate MovieEditorTemplate
+        {
+            get;
+            set;
+        }
+
+        public ShopItemEditorContentTemplateSelector()
+        {
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item == null) 
+            {
+                return null;
+            }
+
+            if (item is Food)
+            {
+                return FoodEditorTemplate;
+            }
+            else if (item is Movie)
+            {
+                return MovieEditorTemplate;
+            }
+            else 
+            {
+                return null;
+            }
+        }
+    }
+}
