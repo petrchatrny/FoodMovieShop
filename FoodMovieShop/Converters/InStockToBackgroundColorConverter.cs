1aef8254b41cfd7ce402493d7625f2c4fa6e25a0
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2021-02-26 16:52:44 +0100

    Implemented converters

---
+++ FoodMovieShop/Converters/InStockToBackgroundColorConverter.cs
@@ -0,0 +1,48 @@
+﻿using FoodMovieShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FoodMovieShop.Converters
+{
+	public class InStockToBackgroundColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var shopItem = (ShopItem)value;
+			if (shopItem == null) 
+			{
+				return null;
+			}
+
+			if (shopItem.InStock <= 0)
+			{
+				return new SolidColorBrush(Colors.Gray);
+			}
+
+			else
+			{
+				if (shopItem is Food)
+				{
+					return new SolidColorBrush(Colors.CornflowerBlue);
+				}
+				else if (shopItem is Movie)
+				{
+					return new SolidColorBrush(Colors.IndianRed);
+				}
+				else
+				{
+					return null;
+				}
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+	}
+}
